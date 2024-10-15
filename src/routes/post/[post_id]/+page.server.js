2c36be336971a3b084497f3fca71e58cebac8c68
xubeiyan <yueyongdong@gmail.com>
2024-10-15 17:47:04 +0800

    fix(src/routes/post/[post_id]/+page.server.js): add post id

--- src/routes/post/[post_id]/+page.server.js
+++ src/routes/post/[post_id]/+page.server.js
@@ -4,7 +4,7 @@ export const load = async ({ locals, params }) => {
 	const { dbconn } = locals;
 
 	const query = {
-		text: `SELECT p.poster_name, p.poster_email, p.title, p.content, 
+		text: `SELECT p.id, p.poster_name, p.poster_email, p.title, p.content, 
             to_char(p.post_timestamp, 'YYYY-MM-DD HH24:MI:SS') AS post_time,
             to_char(p.edit_timestamp, 'YYYY_MM-DD HH24:MI:SS') AS edit_time,
             c.content AS cookies_content 
@@ -14,7 +14,6 @@ export const load = async ({ locals, params }) => {
 	};
 
 	const result = await dbconn.query(query);
-	console.log(result);
 
 	if (result.rowCount != 1) {
 		return {
@@ -22,11 +21,12 @@ export const load = async ({ locals, params }) => {
 		};
 	}
 
-	const { poster_name, poster_email, title, content, post_time, edit_time, cookies_content } =
+	const { id, poster_name, poster_email, title, content, post_time, edit_time, cookies_content } =
 		result.rows[0];
 
 	return {
 		post: {
+			id: id,
 			author: poster_name == 'null' ? '无名氏' : poster_name,
 			email: poster_email == 'null' ? 'no@name.net' : poster_email,
 			title: title == 'null' ? '无标题' : title,
