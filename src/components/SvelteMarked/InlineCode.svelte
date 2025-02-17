1f5f5c4f03a792156abf45921622863df490f5e9
xubeiyan <yueyongdong@gmail.com>
2025-02-17 13:45:12 +0800

    fix(InlineCode.svelte): remove the margin at start/end of inline code

--- src/components/SvelteMarked/InlineCode.svelte
+++ src/components/SvelteMarked/InlineCode.svelte
@@ -2,6 +2,6 @@
 	export let content = '';
 </script>
 
-<code class="shadow-inner rounded-md px-1 py-0.5 bg-gray-300 dark:bg-gray-700 mx-1">
+<code class="rounded-md px-1 py-0.5 bg-gray-300 dark:bg-gray-700">
 	{content}
 </code>
