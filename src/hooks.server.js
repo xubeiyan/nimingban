56a718a49aa71ebb9a81d3d863bf97379f9207b3
xubeiyan <yueyongdong@gmail.com>
2025-09-20 08:08:37 +0800

    fix(Header.svelte): fix need refresh page to clear the outdated login session fix(hooks.server.js): return 404 when chrome requesting dev tools

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,6 +3,8 @@ import { connectToDB } from '$lib/db';
 import { PGHOST, PGUSER, PGDATABASE, PGPASSWORD, PGPORT } from '$env/static/private';
 import { sequence } from '@sveltejs/kit/hooks';
 
+import { dev } from '$app/environment';
+
 const db = async ({ event, resolve }) => {
 	// 在访问install路由时不连接数据库
 	if (event.url.pathname.startsWith('/install')) {
@@ -23,4 +25,13 @@ const db = async ({ event, resolve }) => {
 	dbconn.release();
 	return response;
 };
-export const handle = sequence(db);
+
+// 移除调试时的Chrome的对 /.well-known/appspecific/com.chrome.devtools.json 的傻缺请求
+const shutUpDevTools = ({event, resolve}) => {
+	if (dev && event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
+		return new Response(undefined, { status: 404 });
+	}
+	return resolve(event);
+}
+
+export const handle = sequence(db, shutUpDevTools);
